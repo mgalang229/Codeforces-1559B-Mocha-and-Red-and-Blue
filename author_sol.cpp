108546259cc78876ee9ab3fe7739a5dfb409ebae
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-23 17:03:31 +0800

    Update author_sol.cpp

--- author_sol.cpp
+++ author_sol.cpp
@@ -21,6 +21,7 @@ int main() {
 			// if there are no letters, set the first letter to 'R'
 			s[0] = 'R';
 		}
+		// view the image in this repository for better understanding
 		// create pattern wherein the question marks depend on the letter from the left adjacent side
 		for (int i = 1; i < n; i++) {
 			if (s[i] == '?' && s[i - 1] != '?') {
